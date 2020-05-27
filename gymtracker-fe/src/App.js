3b4878b1e2621b1a551ebbf378b2f6652d3ca67f
mac <mac@macs-MacBook-Pro.local>
2020-05-27 20:58:25 +0300

    React Register Component

--- gymtracker-fe/src/App.js
+++ gymtracker-fe/src/App.js
@@ -1,27 +1,85 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import "bootstrap/dist/css/bootstrap.min.css";
-
-
 import React from 'react';
-import './App.css';
 
-import About from "./Components/about.component"
-import Login from "./Components/login.component";
-import Navigation from "./Components/navbar.component"
-import Register from "./Components/register.component";
+import { BrowserRouter as Router, Switch, Route,Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
+import { Layout } from 'antd';
+
+import './App.css';
+import Register from './User/Register';
 
 function App() {
+  const { t } = useTranslation();
+  const { Content } = Layout;
   return (
-    <Router>
-      <div className="container">
-        <Navigation />
-        <br/>
-        <Route path="/about" component={ About } />
-        <Route path="/login" component={ Login } />
-        <Route path="/register" component={ Register } />
-      </div>
-    </Router>
+    <Layout className="layout">
+      <Content>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">{t('Welcome')}</Link>
+            </li>
+            <li>
+              <Link to="/about">{t('About')}</Link>
+            </li>
+            <li>
+              <Link to="/users">{t('Users')}</Link>
+            </li>
+            <li>
+              <Link to="/register">{t('Register')}</Link>
+            </li>
+          </ul>
+        </nav>
+
+          {/*
+            A <Switch> looks through all its children <Route> elements and renders the first one whose path
+            matches the current URL. Use a <Switch> any time you have multiple routes, but you want only one
+            of them to render at a time
+          */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+          </Switch>
+      </Router>
+      </Content>
+    </Layout>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+function Users() {
+  return (
+    <div>
+      <h1>Users</h1>
+    </div>
   );
 }
 
--- gymtracker-fe/src/Components/about.component.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function About() {
-  return (
-    <div className="form-group">
-        <h3>GymTracker is an app that tracks your progress in the gym and much more.</h3>
-    </div>
-  );
-}
