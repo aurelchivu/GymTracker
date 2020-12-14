94ad98ecf617095d6c3109e7f18f116cc89ddfbf
mac <mac@macs-MacBook-Pro.local>
2020-12-14 10:02:31 +0200

    added redux persist

--- gymtracker-fe/src/index.js
+++ gymtracker-fe/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { Provider } from 'react-redux'
-import store from './store'
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './store'
 
 import './index.css';
 import App from './App';
@@ -18,10 +19,12 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <>
     <Provider store={store}>
-      <Router history={hist}>
-        <App />
-        <Route path="/admin" component={Admin} />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router history={hist}>
+          <App />
+          <Route path='/admin' component={Admin} />
+        </Router>
+      </PersistGate>
     </Provider>
   </>,
   document.getElementById('root')
