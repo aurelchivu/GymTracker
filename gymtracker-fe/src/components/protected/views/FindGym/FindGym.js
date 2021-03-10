faea70ea0a205b3e7285d10a3a8a4173d095e5c8
mac <mac@macs-MacBook-Pro.local>
2021-03-10 19:34:46 +0200

    show markers on nearby gyms location

--- gymtracker-fe/src/components/protected/views/FindGym/FindGym.js
+++ gymtracker-fe/src/components/protected/views/FindGym/FindGym.js
@@ -7,22 +7,19 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
-  Map,
+  InfoWindow,
 } from 'react-google-maps';
 import mapStyles from './mapStyles';
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 export default function Maps() {
   const [currentPosition, setCurrentPosition] = useState({
     lat: null,
     lng: null,
   });
-
   const [gyms, setGyms] = useState([]);
+  const [selectedGym, setSelectedGym] = useState(null);
 
   const { REACT_APP_GOOGLE_MAPS_URL_KEY } = process.env;
-  const googleMapURL =
-    `https://maps.googleapis.com/maps/api/js?key=${REACT_APP_GOOGLE_MAPS_URL_KEY}`;
 
   const CustomSkinMap = withScriptjs(
     withGoogleMap(() => (
@@ -30,15 +27,57 @@ export default function Maps() {
         defaultZoom={12}
         defaultCenter={currentPosition}
         defaultOptions={{
-          scrollwheel: false,
+          scrollwheel: true,
           zoomControl: true,
           styles: mapStyles,
         }}
       >
-        
+        {gyms.map((gym) => (
+          <Marker
+            key={gym.place_id}
+            position={{
+              lat: gym.geometry.location.lat,
+              lng: gym.geometry.location.lng,
+            }}
+            onMouseOver={() => {
+              console.log(gym.name);
+              console.log(gym.formatted_address);
+              // setSelectedGym(gym);
+            }}
+            icon={{
+              url: '/gymIcon.png',
+              scaledSize: new window.google.maps.Size(26, 26),
+            }}
+          />
+        ))}
+
+        {(selectedGym && 
+          <InfoWindow
+            // onCloseClick={() => {
+            //   setSelectedGym(null);
+            // }}
+            getContent
+            position={{
+              lat: selectedGym.geometry.location.lat,
+              lng: selectedGym.geometry.location.lnt,
+            }}
+            content={
+              <div>
+                <h2>SELECTED</h2>
+                <h2>{selectedGym.name}</h2>
+                <p>{selectedGym.formatted_address}</p>
+              </div>
+            }
+          >
+            <div>
+              <h2>SELECTED</h2>
+              <h2>{selectedGym.name}</h2>
+              <p>{selectedGym.formatted_address}</p>
+            </div>
+          </InfoWindow>
+        )}
         <Marker position={currentPosition} />
       </GoogleMap>
-      
     ))
   );
 
@@ -48,8 +87,8 @@ export default function Maps() {
         `http://localhost:5000/api/v1/findgym?location=${currentPosition.lat},${currentPosition.lng}&radius=10000&type=gym&keyword=near&key=${REACT_APP_GOOGLE_MAPS_URL_KEY}`
       );
 
-      console.log(data);
-      setGyms(data.results);
+      console.log(data.data.results);
+      setGyms(data.data.results);
     } catch (error) {
       console.log(error);
     }
@@ -69,13 +108,15 @@ export default function Maps() {
     } else {
       window.alert('Location not found');
     }
-    findGymNearMe();
+    if (currentPosition.lat) {
+      findGymNearMe();
+    }
   }, [currentPosition]);
 
   return (
     <>
       <CustomSkinMap
-        googleMapURL={googleMapURL}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${REACT_APP_GOOGLE_MAPS_URL_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100vh` }} />}
         mapElement={<div style={{ height: `100%` }} />}
